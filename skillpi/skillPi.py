81eb2578c9dff984665c712d9875507936cbc410
Dntwry <rigua001@fiu.edu>
2017-03-17 12:22:55 -0400

    Added connection file

---
+++ skillpi/skillPi.py
@@ -0,0 +1,40 @@
+import socket
+import RPi.GPIO as GPIO
+import time
+import sys
+import os
+from subprocess import Popen
+
+# hit=0
+# miss=0
+
+GPIO.setmode(GPIO.BCM)
+# LED = 18
+hSensor = 11
+# Initialize pins
+# GPIO.setup(LED, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(hSensor, GPIO.IN)
+
+sock = socket.socket()
+
+host = socket.gethostname()
+port = 5000
+sock.bind((host, port))
+
+sock.listen(5)
+while True:
+    con, addr = sock.accept()
+    print('Got connection from', addr)
+    # if the ball hits while led signal is 0
+    if con.recv() and GPIO.input(hSensor) == True:
+        os.system('killall omxplayer.bin')
+        os.system('omxplayer -l hit.mp3')
+        # hit+=1
+    # if ball hits while led signal is 1
+    elif con.rcv() and GPIO.input(hSensor) == True:
+        os.system('killall omxplayer.bin')
+        os.system('omxplayer -l miss.mp3')
+        # miss+=1
+
+
+
