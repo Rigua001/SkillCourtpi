3ea847ed05a2047818ec12916411ef7667f0b59d
Dntwry <rigua001@fiu.edu>
2017-04-12 00:03:09 -0400

    added song mode

--- skillpi/skillPi.py
+++ skillpi/skillPi.py
@@ -7,37 +7,74 @@ from subprocess import Popen
 
 # hit=0
 # miss=0
-
+mode = 15 #15 hit mode, 16 song mode, 17 listen mode
+runcount = 0
 GPIO.setmode(GPIO.BCM)
 # LED = 18
 hSensor = 11
 # Initialize pins
 # GPIO.setup(LED, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(hSensor, GPIO.IN)
+GPIO.setup(hSensor, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 sock = socket.socket()
 
-host = socket.gethostname()
-port = 23
+host = '0.0.0.0'#socket.gethostname()
+port = 5000
 sock.bind((host, port))
 
 sock.listen(5)
-while True:
+#check modes and make fuctions for modes?
+try:
+    while True:
     con, addr = sock.accept()
     print('Got connection from', addr)
-    print(con.recv(1024))
-    os.system('cd /home/pi/SkillCourtStand')
-    # if the ball hits while led signal is 0
-    if con.recv(1024) == '0' and GPIO.input(hSensor) == True:
-        os.system('killall omxplayer.bin')
-        os.system('omxplayer -l hit.mp3')
-        # hit+=1
-    # if ball hits while led signal is 1
-    elif con.rcv(1024) == '1' and GPIO.input(hSensor) == True:
-        os.system('killall omxplayer.bin')
-        os.system('omxplayer -l miss.mp3')
-        # miss+=1
-    elif con.rcv(1024) == '4':
-        sock.close()
+    msg = con.recv(1024)
+    mode = msg.decode('')
+    song= con.recv(1024)
+    songinfo = song.decode('')
+
+
+    while mode == '15': #normal hit miss
+        msg = con.recv(1024)
+        dmsg = msg.decode('')
+        # if the ball hits while led signal is 0
+        if dmsg == '0' and GPIO.input(hSensor) == True:
+            os.system('killall omxplayer.bin')
+            os.system('omxplayer -l hit.mp3')
+            # hit+=1
+        # if ball hits while led signal is 1
+        elif dmsg == '1' and GPIO.input(hSensor) == True:
+            os.system('killall omxplayer.bin')
+            os.system('omxplayer -l miss.mp3')
+            # miss+=1
+        elif dmsg == '-1':
+            mode == '0'
+    while mode == '16': #sound game
+        if runcount == 0:
+            os.system('mpsyt')
+            time.sleep(10)
+            os.system('search' + songinfo)
+            time.sleep(10)
+            os.system('1')
+            runcount = 1
+
+        msg = con.recv(1024)
+        dmsg = msg.decode('')
+        # if the ball hits while led signal is 0 if hit is good, green
+        if dmsg == '0' and GPIO.input(hSensor) == True:
+            #os.system('killall omxplayer.bin')
+            os.system('+')
+            # hit+=1
+        # if ball hits while led signal is 1, if hit is bad, red
+        elif dmsg == '1' and GPIO.input(hSensor) == True:
+            #os.system('killall omxplayer.bin')
+            os.system('-')
+            # miss+=1
+        elif dmsg == '-1':
+            mode == '0'
+finally:
+    sock.close()
+
+
 
 
